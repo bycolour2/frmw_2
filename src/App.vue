157abf29c2bd78aee67af3f60e76ea51360d5d8e
encore123 <ruskir53@gmail.com>
2020-07-09 18:51:30 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div>
+      <input v-model="count" />
+      <input v-model="text" />
+    </div>
+    <div>
+      <template v-for="i in +count">
+        <input :value="text" v-bind:key="i" />
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      count: "",
+      text: "",
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
